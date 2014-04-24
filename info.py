56edb5d5285f295b07c0cbfe51966accd02b81a9
Henry Hu <henry.hu.sh@gmail.com>
2014-04-24 19:10:47 -0400

    parse user info

--- info.py
+++ info.py
@@ -1,2 +1,10 @@
 def update_state(state, config_node):
-    pass
+    for child in config_node:
+        if child.tag == 'nick_name':
+            state['user_nick'] = child.text
+        elif child.tag == 'user_id':
+            state['user_id'] = child.text
+        elif child.tag == 'vip':
+            state['vip'] = child.text
+        elif child.tag == 'vip_role':
+            state['vip_role'] = child.text
