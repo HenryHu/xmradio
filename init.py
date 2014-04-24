472e482529e20cc47b967a77337f1afc08926908
Henry Hu <henry.hu.sh@gmail.com>
2014-04-24 18:57:18 -0400

    fake user agent, or radio list returns 503

--- init.py
+++ init.py
@@ -5,6 +5,7 @@ import logging
 def init():
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = (('User-agent', 'Mozilla/5.0 ( X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0'),)
     urllib2.install_opener(opener)
 
     logging.basicConfig(level=logging.DEBUG)
